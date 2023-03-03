150dc85ccaec16514c28e14d5f3947a44c117f33
danil-rezbaev <92051977+danil-rezbaev@users.noreply.github.com>
2023-03-03 20:38:11 +0300

    fix: in Post updated imageUrl

--- src/components/Post/index.jsx
+++ src/components/Post/index.jsx
@@ -61,7 +61,8 @@ export const Post = ({
       {imageUrl && (
         <img
           className={clsx(styles.image, { [styles.imageFull]: isFullPost })}
-          src={`${process.env.REACT_APP_API_URL}/${imageUrl}`}
+          src={imageUrl}
+          // src={`${process.env.REACT_APP_API_URL}/${imageUrl}`}
           alt={title}
         />
       )}
