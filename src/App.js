1c35a58ee92539216e5259f6da3ab70490548d87
danil-rezbaev <92051977+danil-rezbaev@users.noreply.github.com>
2023-03-03 18:53:35 +0300

    feat: Created routes in App.js

--- src/App.js
+++ src/App.js
@@ -1,18 +1,23 @@
 import Container from "@mui/material/Container";
-
 import { Header } from "./components";
 import { Home, FullPost, Registration, AddPost, Login } from "./pages";
+import {Routes, Route} from 'react-router-dom'
+import { SearchOnTag } from "./pages/SearchOnTag";
 
 function App() {
   return (
     <>
       <Header />
       <Container maxWidth="lg">
-        <Home />
-        {/*<Fullgit remote add origin https://github.com/danil-rezbaev/blog--frontend.gitPost />*/}
-        {/*<AddPost />*/}
-        {/*<Login />*/}
-        {/*<Registration />*/}
+        <Routes>
+          <Route index path='/' element={<Home/>}/>
+          <Route path='/posts/:id/edit' element={<AddPost/>}/>
+          <Route path='/posts/:id' element={<FullPost/>}/>
+          <Route path='/add-post' element={<AddPost/>}/>
+          <Route path='/login' element={<Login/>}/>
+          <Route path='/register' element={<Registration/>}/>
+          <Route path='/tags/:tagName' element={<SearchOnTag/>}/>
+        </Routes>
       </Container>
     </>
   );
